ae48a1f8bd0548eb3923e8ac3756dfb052791e5a
Samuel Masson <smasson99@gmail.com>
2018-09-13 10:48:19 -0400

    ajout du labirynte

---
+++ Navmesh Testing/NavmeshProject/Assets/Scripts/Scripts/GravityConstantForceScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityConstantForceScript : MonoBehaviour
+{
+    [Tooltip("Gravity force to apply to the GameObject.")]
+    [SerializeField]
+    private float gravityForce = 1;
+
+    private bool isDebuging;
+    private CharacterController characterController;
+    private Vector3 direction;
+
+    private void Awake()
+    {
+        this.characterController = GetComponent<CharacterController>();
+        direction = new Vector3(0, -gravityForce, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        characterController.SimpleMove(direction);
+    }
+
+    private void SayToLog(string message)
+    {
+        if (isDebuging)
+        {
+            Debug.Log(message);
+        }
+    }
+}
